31278b4b46fa6873771b5bf229b14222a437ed58
eyuel481 <joybekure@gmail.com>
2026-03-02 17:54:15 +0300

    Add service worker event listeners for install and activate

--- sw.js
+++ sw.js
@@ -23,3 +23,11 @@ self.addEventListener('message', (event) => {
         }, delay);
     }
 });
+// sw.js
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow('/')); 
+});
