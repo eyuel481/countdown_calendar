a67a373b639bb0abee8e54f7cd2201a4ca45bfa6
eyuel481 <joybekure@gmail.com>
2026-03-02 17:45:24 +0300

    Add service worker event listeners for notifications

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
 self.addEventListener('fetch', (event) => {
   // This is a basic shell to allow installation
 });,
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow('/')); // Opens your app when clicked
+});
+
+// Listener for the background timer
+self.addEventListener('message', (event) => {
+    if (event.data.type === 'SCHEDULE_NOTIFICATION') {
+        const { title, body, delay } = event.data;
+        setTimeout(() => {
+            self.registration.showNotification(title, {
+                body: body,
+                icon: 'icon.png', // Make sure you have an icon file!
+                badge: 'icon.png'
+            });
+        }, delay);
+    }
+});
